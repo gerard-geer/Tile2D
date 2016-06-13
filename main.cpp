4065e071c8a40bd90d5815fedfcbac2d9635ef73
gerard.geer <gerarddgeer@gmail.com>
2016-06-13 06:22:47 -0500

    Added sample textures to repository and changed paths in main to point to them.

--- main.cpp
+++ main.cpp
@@ -10,23 +10,23 @@ int main(int argc, char **argv)
     window.create(1000, 716, 256, 240, (char*)"WOO");
     Renderer * r = window.getRenderer();
 
-    r->getAssetManager()->addNewTexture((char*)"pups", (char*)"/home/gerard/Pictures/mrpups.png");
+    r->getAssetManager()->addNewTexture((char*)"puppy", (char*)"../Assets/Rendering Assets/Textures/puppy.png");
     
-    r->getAssetManager()->addNewTexture((char*)"kitten", (char*)"/home/gerard/Pictures/kitten512.png");
+    r->getAssetManager()->addNewTexture((char*)"kitten", (char*)"../Assets/Rendering Assets/Textures/kitten.png");
     
-    r->getAssetManager()->addNewTexture((char*)"travis", (char*)"/home/gerard/Pictures/travis.PNG");
+    r->getAssetManager()->addNewTexture((char*)"fish", (char*)"../Assets/Rendering Assets/Textures/fish.png");
     
-    r->getAssetManager()->addNewTexture((char*)"yumetarou", (char*)"/home/gerard/Pictures/yumetarou_frame16x20.png");
+    r->getAssetManager()->addNewTexture((char*)"yumetarou", (char*)"../Assets/Rendering Assets/Textures/yumetarou_frame16x20.png");
     
     r->getAssetManager()->addNewShader((char*)"example_post_shader", (char*)"../Assets/Rendering Assets/Shaders/ex_post_tile_shader.vert",
                                                            (char*)"../Assets/Rendering Assets/Shaders/ex_post_tile_shader.frag");
-    BGTile * bg = r->makeBGTile(0,0,1.5,1.5,false,(char*)"pups");
+    BGTile * bg = r->makeBGTile(0,0,1.5,1.5,false,"puppy");
     r->addToRenderQueue(BG_TILE, bg);
     
-    PostTile * pt = r->makePostTile(.025, .25, PLANE_POS_1, .5, .5, false, (char*)"pups", (char*)"kitten", NULL, NULL, (char*)"example_post_shader");
+    PostTile * pt = r->makePostTile(.025, .25, PLANE_POS_1, .5, .5, false, (char*)"kitten", (char*)"kitten", NULL, NULL, (char*)"example_post_shader");
     r->addToRenderQueue(POST_TILE, pt);
     
-    SceneTile * st = r->makeSceneTile(.25, 0, PLANE_NEG_2, .5, .5, false, (char*)"travis");
+    SceneTile * st = r->makeSceneTile(.25, 0, PLANE_NEG_2, .5, .5, false, (char*)"fish");
     r->addToRenderQueue(SCENE_TILE, st);
     
     AnimTile * at = r->makeAnimTile(100.0, 40.0, PLANE_PLAYFIELD_A, 16.0, 20.0, true, (char*)"yumetarou", 6, 16, 20, 1.0/20.0);
@@ -44,4 +44,3 @@ int main(int argc, char **argv)
     }
     return 0;
 }
-    
