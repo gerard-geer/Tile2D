fde9981068979c0fc2307722f90c7280d10778bc
gerard.geer <gerarddgeer@gmail.com>
2016-06-21 18:07:55 -0500

    We now pull out the AssetManager instance and use it, instead of calling getAssetManager() each time we use it.

--- main.cpp
+++ main.cpp
@@ -7,20 +7,20 @@ using namespace std;
 int main(int argc, char **argv)
 {
     Window window = Window();
-    window.create(1000, 716, 256, 224, (char*)"WOO");
+    window.create(1000, 700, 256, 224, (char*)"WOO");
     Renderer * r = window.getRenderer();
-
-    r->getAssetManager()->addNewTexture((char*)"puppy", (char*)"../Assets/Rendering Assets/Textures/puppy.png");
+    AssetManager * a = r->getAssetManager();
+    a->addNewTexture((char*)"puppy", (char*)"../Assets/Rendering Assets/Textures/puppy.png");
     
-    r->getAssetManager()->addNewTexture((char*)"kitten", (char*)"../Assets/Rendering Assets/Textures/kitten.png");
+    a->addNewTexture((char*)"kitten", (char*)"../Assets/Rendering Assets/Textures/kitten.png");
     
-    r->getAssetManager()->addNewTexture((char*)"fish", (char*)"../Assets/Rendering Assets/Textures/fish.png");
+    a->addNewTexture((char*)"fish", (char*)"../Assets/Rendering Assets/Textures/fish.png");
     
-    r->getAssetManager()->addNewTexture((char*)"yumetarou", (char*)"../Assets/Rendering Assets/Textures/yumetarou_frame16x20.png");
+    a->addNewTexture((char*)"yumetarou", (char*)"../Assets/Rendering Assets/Textures/yumetarou_frame16x20.png");
     
-    r->getAssetManager()->addNewShader((char*)"example_post_shader", (char*)"../Assets/Rendering Assets/Shaders/ex_post_tile_shader.vert",
+    a->addNewShader((char*)"example_post_shader", (char*)"../Assets/Rendering Assets/Shaders/ex_post_tile_shader.vert",
                                                            (char*)"../Assets/Rendering Assets/Shaders/ex_post_tile_shader.frag");
-    BGTile * bg = r->makeBGTile(0,0,1.5,1.5,false,"puppy");
+    BGTile * bg = r->makeBGTile(0.0, 0.0, 1.5, 1.5, false, "puppy");
     r->addToRenderQueue(BG_TILE, bg);
     
     PostTile * pt = r->makePostTile(.025, .25, PLANE_POS_1, .5, .5, false, (char*)"kitten", (char*)"kitten", NULL, NULL, (char*)"example_post_shader");
@@ -31,10 +31,11 @@ int main(int argc, char **argv)
     
     AnimTile * at = r->makeAnimTile(100.0, 40.0, PLANE_PLAYFIELD_A, 16.0, 20.0, true, (char*)"yumetarou", 6, 16, 20, 1.0/20.0);
     r->addToRenderQueue(ANIM_TILE, at);
-    
+
     while(!glfwWindowShouldClose(window.getWindow()))
     {
         window.update();
     }
+    window.destroy();
     return 0;
 }
