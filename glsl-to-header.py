1cb9802778506e3ee79ecd773ad37e47ae61c227
Gerard Geer <gerarddgeer@gmail.com>
2016-07-18 04:51:25 -0500

    Started a script to embed glsl source into #define macros within a header file.

---
+++ glsl-to-header.py
@@ -0,0 +1,50 @@
+#!/usr/local/bin/python
+
+import sys
+
+"""
+File: glsl-to-header.py
+Author: Gerard Geer (gerarddgeer@gmail.com)
+
+This script simply takes the filenames passed to it as arguments, and
+spits out their contents into several #define directives in a single
+file.
+
+usage:
+    glsl-to-header <output filepath> <filepath A> <filepath B> ...
+"""
+
+"""
+Opens a given filename, and stores its contents as a #define macro
+within a returned String.
+
+Parameters:
+    name (String): The name of the directive.
+    file (String): The filepath of the file to #define-ify.
+"""
+def createDefineDirective(name, file):
+
+    # Start off the define macro.
+    result = "#define " + str(name) + " "
+    
+    # Open the file.
+    f = open(file)
+    
+    # Go through the file appending each line to the macro.
+    for line in f:
+        result += '"' + str(line) + '"  \\\n'
+     
+    # Finally we return the resultant macro.
+    return result
+
+"""
+Simply prints usage methodology.
+
+Parameters: None.
+
+Returns: None.
+"""
+def printUsage():
+    print("usage:")
+    print("\tglsl-to-header <output filepath> <filepath A> <filepath B> ...")
+        
