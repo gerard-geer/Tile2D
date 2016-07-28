e5fd54720f19c9bcba63d7ceb2d51b14a6af7f0f
Gerard Geer <gerarddgeer@gmail.com>
2016-07-28 03:39:59 -0500

    Updated documentation.

--- include/Shader.h
+++ include/Shader.h
@@ -123,7 +123,8 @@ public:
     
     /**
      * @brief Creates a shader from two strings containing source code, rather than
-     *		  loading it from file.
+     *		  loading it from file. Note, this expects the strings to be formatted
+     * 		  as by glsl-to-header, with a $ at the end of every line.
      * @param vertSource The source code string of the vertex shader.
      * @param fragSource The source code string of the fragment shader.
      * @return A shader_error, if any.
