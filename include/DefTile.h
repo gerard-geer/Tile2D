4cae04776b0732e8226cb5fb36411fe238693b76
Gerard Geer <gerarddgeer@gmail.com>
2016-08-22 09:38:09 -0500

    Renamed PostTile.* to DefTile.*, and updated all code to suit.

--- include/DefTile.h
+++ include/DefTile.h
@@ -1,5 +1,5 @@
-#ifndef POSTTILE_H
-#define POSTTILE_H
+#ifndef DEFTILE_H
+#define DEFTILE_H
 
 #include "Shader.h"
 #include "Tile.h"
@@ -8,11 +8,11 @@
 #include "BasicMatrix.h"
 
 /**
- * @class PostTile
+ * @class DefTile
  * @author Gerard Geer
  * @date 06/13/16
- * @file PostTile.h
- * @brief A PostTile, short for Post-processing Tile. This variety of Tile
+ * @file DefTile.h
+ * @brief A DefTile, short for Post-processing Tile. This variety of Tile
  *        allows you to specify up to four custom Textures and have a custom
  *        shader (GLSL 1.2) as well. The shader can only access certain uniforms
  *        though:
@@ -29,7 +29,7 @@
  *        texC: The third custom texture. (sampler2D)
  *        texD: The fourth custom texture. (sampler2D)
  */
-class PostTile : public Tile
+class DefTile : public Tile
 {
 private:
     /*
@@ -41,45 +41,45 @@ private:
     char * texD;
     
     /*
-     * The key to the PostTile's custom shader.
+     * The key to the DefTile's custom shader.
      */
     char * shader;
 public:
     
     /**
-     * @brief Constructs a new PostTile. Don't forget to call init()!
+     * @brief Constructs a new DefTile. Don't forget to call init()!
      */
-    PostTile();
+    DefTile();
     /**
-     * @brief Destructs this PostTile. Don't forget to call destroy() first!
+     * @brief Destructs this DefTile. Don't forget to call destroy() first!
      */
-    ~PostTile();
+    ~DefTile();
     
     /**
-     * @brief Initializes this PostTile. This is a hefty one. It sets up the
+     * @brief Initializes this DefTile. This is a hefty one. It sets up the
      *        internal BasicMatrix pointer, as well as defines the shader and
      *        all the textures to be used. 
      *        To not use a texture slot, just pass in NULL. Note though, that 
      *        the corresponding uniform must still be present in the shader. 
      *        The only difference is that an empty texture will be passed to 
      *        it. 
-     *        Also realize that since PostTiles use bespoke shaders X, Y, plane, 
+     *        Also realize that since DefTiles use bespoke shaders X, Y, plane, 
      *        width and height may be ignored by the shader. 
      *        Also because of the custom shaders, it's assumed that these will
      *        have transparency. 
      *        Finally, remember that the shader and textures must already be 
      *        added to the Renderer's Asset Manager.
      * 
-     * @param x The X position of this PostTile.
-     * @param y The Y position of this PostTile.
-     * @param plane The parallax plane to draw this PostTile to.
-     * @param width The width of this PostTile.
-     * @param height The height of this PostTile.
-     * @param texA The first custom texture that this PostTile uses.
-     * @param texB The second custom texture that this PostTile uses.
-     * @param texC The third custom texture that this PostTile uses.
-     * @param texD The fourth custom texture that this PostTile uses.
-     * @param shader This PostTile's shader.
+     * @param x The X position of this DefTile.
+     * @param y The Y position of this DefTile.
+     * @param plane The parallax plane to draw this DefTile to.
+     * @param width The width of this DefTile.
+     * @param height The height of this DefTile.
+     * @param texA The first custom texture that this DefTile uses.
+     * @param texB The second custom texture that this DefTile uses.
+     * @param texC The third custom texture that this DefTile uses.
+     * @param texD The fourth custom texture that this DefTile uses.
+     * @param shader This DefTile's shader.
      */
     void init(GLfloat x, GLfloat y, tile_plane plane, GLfloat width, 
               GLfloat height, char * texA, char * texB,
@@ -92,64 +92,64 @@ public:
     void render(Renderer * r);
     
     /**
-     * @brief Changes the key used to acces this PostTile's shader.
+     * @brief Changes the key used to acces this DefTile's shader.
      * @param shader The new key.
      */
     void setShader(char* shader);
     
     /**
-     * @brief Changes the key used to access this PostTile's first texture.
+     * @brief Changes the key used to access this DefTile's first texture.
      * @param texA The new key.
      */
     void setTexA(char* texA);
     
     /**
-     * @brief Changes the key used to access this PostTile's second texture.
+     * @brief Changes the key used to access this DefTile's second texture.
      * @param texA The new key.
      */
     void setTexB(char* texB);
     
     /**
-     * @brief Changes the key used to access this PostTile's third texture.
+     * @brief Changes the key used to access this DefTile's third texture.
      * @param texA The new key.
      */
     void setTexC(char* texC);
     
     /**
-     * @brief Changes the key used to access this PostTile's fourth texture.
+     * @brief Changes the key used to access this DefTile's fourth texture.
      * @param texA The new key.
      */
     void setTexD(char* texD);
     
     /**
-     * @brief Returns the key used to access this PostTile's shader.
-     * @return The key used to access this PostTile's Shader.
+     * @brief Returns the key used to access this DefTile's shader.
+     * @return The key used to access this DefTile's Shader.
      */
     char * getShader();
     
     /**
-     * @brief Returns the key used to access this PostTile's first custom texture.
-     * @return The key used to access this PostTile's first custom texture.
+     * @brief Returns the key used to access this DefTile's first custom texture.
+     * @return The key used to access this DefTile's first custom texture.
      */
     char * getTexA();
     
     /**
-     * @brief Returns the key used to access this PostTile's second custom texture.
-     * @return The key used to access this PostTile's second custom texture.
+     * @brief Returns the key used to access this DefTile's second custom texture.
+     * @return The key used to access this DefTile's second custom texture.
      */
     char * getTexB();
     
     /**
-     * @brief Returns the key used to access this PostTile's second custom texture.
-     * @return The key used to access this PostTile's second custom texture.
+     * @brief Returns the key used to access this DefTile's second custom texture.
+     * @return The key used to access this DefTile's second custom texture.
      */
     char * getTexC();
     
     /**
-     * @brief Returns the key used to access this PostTile's second custom texture.
-     * @return The key used to access this PostTile's second custom texture.
+     * @brief Returns the key used to access this DefTile's second custom texture.
+     * @return The key used to access this DefTile's second custom texture.
      */
     char * getTexD();
 };
 
-#endif // POSTTILE_H
+#endif // DefTile_H
